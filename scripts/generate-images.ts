bfae909568a6890d52d780fe46176e740c920696
Dustin Savery <dusave@github.com>
2022-08-24 17:40:13 -0600

    adding file generator

--- scripts/generate-images.ts
+++ scripts/generate-images.ts
@@ -2,31 +2,30 @@ import path from "path";
 import fs from 'fs'
 import sharp from "sharp";
 
-const generateImages =  () => {
-  const galleriesPath = path.join(process.cwd(), `public/galleries`);
-  const paths = fs.readdirSync(galleriesPath, "utf8")
-  paths.filter(x => !x.startsWith("."))
-    .forEach(async (gid) => {
-      const gallery = path.join(process.cwd(), `public/galleries/${gid}`);
-      const files = fs.readdirSync(gallery, "utf8")
-      files.filter(x => x.endsWith(".jpeg"))
-        .forEach(async (pid) => {
-          const imageFile = path.join(process.cwd(), `public/galleries/${gid}/${pid}`);
-          const img = sharp(imageFile);
-          ['sm', 'lg'].forEach(async (size) => {
-            switch(size) {
-              case 'sm': img.resize(600, 600, {fit: 'inside'}); break;
-              case 'lg': img.resize(1400, 1400, {fit: 'inside'}); break;
-            }
+const galleriesPath = path.join(process.cwd(), `public/galleries`);
+const paths = fs.readdirSync(galleriesPath, "utf8")
+paths.filter(x => !x.startsWith("."))
+  .forEach(async (gid) => {
+    const gallery = path.join(process.cwd(), `public/galleries/${gid}`);
+    const files = fs.readdirSync(gallery, "utf8")
+    files.filter(x => x.endsWith(".jpeg"))
+      .forEach(async (pid) => {
+        const imageFile = path.join(process.cwd(), `public/galleries/${gid}/${pid}`);
+        const img = sharp(imageFile);
+        ['sm', 'lg'].forEach(async (size) => {
+          switch(size) {
+            case 'sm': img.resize(600, 600, {fit: 'inside'}); break;
+            case 'lg': img.resize(1400, 1400, {fit: 'inside'}); break;
+          }
+          if(!fs.existsSync(path.join(process.cwd(), `public/galleries/${gid}/${size}`))) {
+            fs.mkdirSync(path.join(process.cwd(), `public/galleries/${gid}/${size}`));
+          }
 
-            img.toFile(path.join(process.cwd(), `public/galleries/${gid}/${size}/${pid}.jpeg`), (err) => {
-              if(err) {
-                console.error(err);
-              }
-            })
+          img.toFile(path.join(process.cwd(), `public/galleries/${gid}/${size}/${pid}.jpeg`), (err) => {
+            if(err) {
+              console.error(err);
+            }
           })
         })
-  })
-}
-
-export default generateImages;
+      })
+})
